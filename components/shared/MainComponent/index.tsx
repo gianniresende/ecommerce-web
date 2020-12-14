91b72a1c92baabf270fccdc641f864ed7b8849a3
gianniresende <gianniresende@gmail.com>
2020-12-13 22:06:41 -0300

    feat: footer

--- components/shared/MainComponent/index.tsx
+++ components/shared/MainComponent/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../Header/StorefrontHeader';
+import Footer from '../Footer/StorefrontFooter';
 
 const MainComponent: React.FC = ({ children }) => {
     return (        
         <div className="d-flex flex-column sticky-footer-wrapper">
           <Header />
           { children }
-          Footer
+          <Footer />
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import MainComponent from '../components/shared/MainComponent';
 
 const Home:React.FC = () => {
   return (
-    <h1>Hello World</h1>
+    <MainComponent>
+      <h1>Home Page</h1>
+
+      {/* <button onClick={() => router.push('/Auth/Login')}>Login</button> */}
+    </MainComponent>
   )
 }
 
