c6071c9f7200984e49c513f4aaeace83cb2ae702
gianniresende <gianniresende@gmail.com>
2020-12-20 20:32:21 -0300

    feat: add reducers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,22 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../store';
 
 const MyApp:React.FC<AppProps> = ({Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <title>OneBitCode</title>
-        <meta name="viewport" content="inital-scale=1.0, width=divice-width" />
-      </Head>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Head>
+            <title>OneBitCode</title>
+            <meta name="viewport" content="inital-scale=1.0, width=divice-width" />
+          </Head>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
     </>
   ) 
 }
