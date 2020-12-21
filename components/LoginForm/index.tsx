6d915b889532bf5ed4845383fd83d38d4c146942
gianniresende <gianniresende@gmail.com>
2020-12-21 09:16:49 -0300

    feat: create authentication part 2

--- components/LoginForm/index.tsx
+++ components/LoginForm/index.tsx
@@ -16,80 +16,90 @@ interface LoginProps {
   titlePhrase: String,
   buttonPhrase: String
 }
-
-const router = useRouter();
-const dispatch = useDispatch();
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-
-const handleSubmit = async (evt: React.FormEvent): Promise<void> => {
-  evt.preventDefault();
-
-  try {
-    const response = await UsersService.signIn({ email, password });
-
-    const { id, email: userEmail, name, profile } = response.data.data;
-
-    const user = {
-      id,
-      name,
-      email: userEmail,
-      profile: profile
-    };
-
-    dispatch(setLoggedUser(user));
-
-    toast.info('Login realizado com sucesso!');
-
-    router.push(user.profile === 'admin' ? '/Admin/' : '/')
-  } catch (err) {
-    toast.error('E-mail ou senha inválidos!');
-  }
-}
-
 const LoginForm: React.FC<LoginProps> = ({titlePhrase, buttonPhrase}) => {
-    return (
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const loggedUser: User = useSelector((state: AuthState) => state.auth.loggedUser);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const passwordRef = useRef(null);
+
+  useEffect(() => {
+    if(loggedUser) {
+      setEmail(loggedUser.email);
+      if(passwordRef && passwordRef.current) {
+        passwordRef.current.focus();
+      }
+    }
+  }, [loggedUser])
+
+  const handleSubmit = async (evt: React.FormEvent): Promise<void> => {
+    evt.preventDefault();
+  
+    try {
+      const response = await UsersService.signIn({ email, password });
+  
+      const { id, email: userEmail, name, profile } = response.data.data;
+  
+      const user = {
+        id,
+        name,
+        email: userEmail,
+        profile: profile
+      };
+  
+      dispatch(setLoggedUser(user));
+  
+      toast.info('Login realizado com sucesso!');
+  
+      router.push(user.profile === 'admin' ? '/Admin/' : '/')
+    } catch (err) {
+      toast.error('E-mail ou senha inválidos!');
+    }
+  }
+
+  return (    
+    <form onSubmit={handleSubmit}>
       <Row>
-        <form onSubmit={handleSubmit}>
-          <Col lg={{span: 6, offset: 3}} md={{span: 8, offset: 2}}>
-          <BlueBackground>
-              <h4>{ titlePhrase }</h4>
+        <Col lg={{span: 6, offset: 3}} md={{span: 8, offset: 2}}>
+        <BlueBackground>
+            <h4>{ titlePhrase }</h4>
 
-              <InputGroup className="mt-3">
-                <FormControl
-                  placeholder="Meu e-mail"
-                  value={email}
-                  type="email"
-                  onChange={
-                      (evt: React.ChangeEvent<HTMLInputElement>) =>
-                      setEmail(evt.target.value)
-                  }
-                  required
-                />
-              </InputGroup>
+            <InputGroup className="mt-3">
+              <FormControl
+                placeholder="Meu e-mail"
+                value={email}
+                type="email"
+                onChange={
+                    (evt: React.ChangeEvent<HTMLInputElement>) =>
+                    setEmail(evt.target.value)
+                }
+                required
+              />
+            </InputGroup>
 
-              <InputGroup className="mt-3">
-                <FormControl
-                  placeholder="Senha"
-                  value={password}
-                  type="password"
-                  onChange={
-                      (evt: React.ChangeEvent<HTMLInputElement>) =>
-                      setPassword(evt.target.value)
-                  }
-                  required
-                />
-              </InputGroup>
+            <InputGroup className="mt-3">
+              <FormControl
+                placeholder="Senha"
+                value={password}
+                type="password"
+                onChange={
+                    (evt: React.ChangeEvent<HTMLInputElement>) =>
+                    setPassword(evt.target.value)
+                }
+                required
+              />
+            </InputGroup>
 
-              <Button className="btn btn-info mt-3 w-100">{buttonPhrase}</Button>            
-              <br /><br />
+            <Button type="submit" className="btn btn-info mt-3 w-100">{buttonPhrase}</Button>            
+            <br /><br />
 
-              <Link href="/Auth/PasswordRecovery">Esqueci minha senha</Link> <br />
-            </BlueBackground>
-          </Col>
-        </form>
-      </Row>
-    )
+            <Link href="/Auth/PasswordRecovery">Esqueci minha senha</Link> <br />
+          </BlueBackground>
+        </Col>
+        </Row>
+    </form>    
+  )
 }
 
 export default LoginForm;
--- pages/Auth/Login/index.tsx
+++ pages/Auth/Login/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import MainComponent from '../../../components/shared/MainComponent';
 import LoginForm from '../../../components/LoginForm';
+import SignUpForm from '../../../components/SignUpForm';
 
 const Login: React.FC = () => {
   return (
     <MainComponent>
       <div className="p-4 text-center">
         <LoginForm titlePhrase="Acessar minha conta" buttonPhrase="ACESSAR" />
+        <br />
+        <SignUpForm titlePhrase="Criar nova conta" buttonPhrase="CRIAR" />
       </div>
     </MainComponent>
   )
